dd946cd836fd94cb7ab0e03902ffe9eba4abb2bd
Tobias Westergaard Kjeldsen <tobias@wkjeldsen.dk>
2020-07-13 18:40:37 +0200

    Nissan Carwings API new endpoint 13 July 2020

--- lib/src/carwings_session.dart
+++ lib/src/carwings_session.dart
@@ -6,7 +6,7 @@ import 'package:dartcarwings/src/carwings_vehicle.dart';
 enum CarwingsRegion { World, USA, Europe, Canada, Australia, Japan }
 
 class CarwingsSession {
-  final String baseUrl = 'https://gdcportalgw.its-mo.com/api_v190426_NE/gdc/';
+  final String baseUrl = 'https://gdcportalgw.its-mo.com/api_v200413_NE/gdc/';
 
   // Result of the call to InitialApp.php, which appears to
   // always be the same.  It'll probably break at some point but
